c66bae6acb792c2e07ca6f9d32ea9b24a436fb6c
Leonardo <leonardo.dellamatrice@gmail.com>
2024-10-01 15:54:49 -0300

    paginas login e inscrição

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, redirect, url_for, render_template, request
+import os
 app = Flask(__name__)
 
 
@@ -7,21 +7,45 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    if request.method == "GET":
+        return render_template('upload.html')
+    elif request.method == "POST":
+        file = request.files["file"]
+        caminho = app.config["UPLOAD_FOLDER"] + "/" + file.filename
+        file.save(caminho)
+        return render_template("produtos.html")
+
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
 
 @app.route('/criar')
 def criar():
     return render_template('criar.html')
 
+
 @app.route('/produtos')
 def produtos():
     return render_template('produtos.html')
 
-@app.route('/login')
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        usuario = request.form.get("usuario")
+        senha = request.form.get("senha")
+
+        if usuario == "maclabs":
+            if senha == "1234":
+                return redirect(url_for('upload'))
+
+        return render_template("login.html")
 
 @app.route('/inscricao')
 def inscricao():
