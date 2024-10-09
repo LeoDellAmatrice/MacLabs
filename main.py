65e957855753dfe146b409fd07c61a88a5d71f48
leonardo <Leonardo.dellamatrice@gmail.com>
2024-10-08 21:10:53 -0300

    produtos

--- main.py
+++ main.py
@@ -33,15 +33,15 @@ def criar():
 @app.route('/produtos')
 def produtos():
     lista_produtos = [
-        {'nome': 'Prato para vaso', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/Pote_flor.svg'},
-        {'nome': 'caixa empilhavel', 'preco': 'R$ 150,00', 'imagem': 'img/produtos/caixa_empilhavel.svg'},
-        {'nome': 'caixa com divisorias empilhavel', 'preco': 'R$ 200,00', 'imagem': 'img/produtos/caixa_divisoria.svg'},
+        {'nome': 'Prato para vaso', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/pote_flor.svg'},
+        {'nome': 'Caixa empilhavel', 'preco': 'R$ 150,00', 'imagem': 'img/produtos/caixa_empilhavel.svg'},
+        {'nome': 'Caixa com divisorias empilhavel', 'preco': 'R$ 200,00', 'imagem': 'img/produtos/caixa_divisoria.svg'},
         {'nome': 'Dispenser de pilhas AA e AAA', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/dispenser_pilha.png'},
         {'nome': 'Funil', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/funil.png'},
         {'nome': 'Garfo', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/garfo.png'},
         {'nome': 'Mesas e cadeiras', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/mesas_cadeiras.png'},
         {'nome': 'Porta lápis', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/porta_lapis.png'},
-        {'nome': 'shuriken', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/shuriken.png'},
+        {'nome': 'Shuriken', 'preco': 'R$ 100,00', 'imagem': 'img/produtos/shuriken.png'},
         # Adicione mais produtos conforme necessário
     ]
 
